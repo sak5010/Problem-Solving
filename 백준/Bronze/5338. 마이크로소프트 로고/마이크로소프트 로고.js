efe322d3f6a5bd39835731cec5ef35ec1de3dd64
SangHyeon <38302831+sak5010@users.noreply.github.com>
2023-01-17 17:27:23 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 120 ms, Memory: 9316 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const fs = require("fs");
+let input = fs.readFileSync('/dev/stdin').toString().trim();
+
+console.log(`       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|`)
