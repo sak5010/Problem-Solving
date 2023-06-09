78f96a2f2cd693c5d8943b7a2f30bab793c3c38b
SangHyeon <38302831+sak5010@users.noreply.github.com>
2023-06-09 16:49:16 +0900

    [Silver I] Title: 그림, Time: 252 ms, Memory: 20536 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.js
+++ 백준/Silver/1926. 그림/그림.js
@@ -1,43 +1,46 @@
+"use strict";
 const fs = require("fs");
 let input = fs.readFileSync('/dev/stdin').toString().trim();
 
+// 주어진 2차원 배열에서 동서남북으로 bfs를 돌린다. 범위를 벗어나지 않도록 한다.
+// 한번 방문한 곳은 2를 넣도록 하자.
+// bfs를 한 번 완수 했을 때 그림의 개수를 1 증가시키고 그 크기를 max와 비교하여 초기화 하자.
 input = input.split("\n");
-const [n, m] = input.shift().split(" ").map(Number);
-const sketchbook = input.map((line) => line.split(" ").map(Number));
+input.shift();
 
 const dx = [0, 0, 1, -1];
 const dy = [1, -1, 0, 0];
+const painting = input.map((line) => line.split(" ").map(Number));
 
-const queue = []; // queue class로 속도 개선 가능
+let countPainting = 0;
+let maxSizePainting = 0;
 
-let countOfPaints = 0;
-let maxSizePaint = 0;
-let sizeOfPaint;
+const n = painting.length;
+const m = painting[0].length;
+
+const queue = [];
 
 for (let x = 0; x < n; x++) {
   for (let y = 0; y < m; y++) {
-    if (sketchbook[x][y] === 1) {
-      sizeOfPaint = 0;
-      countOfPaints++;
-      sizeOfPaint++;
-      sketchbook[x][y] = 2; // 방문
+    if (painting[x][y] === 1) {
+      countPainting++;
+      let sizePainting = 1;
+      painting[x][y] = 2;
       queue.push([x, y]);
       while (queue.length !== 0) {
-        const [queuingX, queuingY] = queue.shift();
+        const [queuedX, queuedY] = queue.shift();
         for (let dir = 0; dir < 4; dir++) {
-          dirX = queuingX + dx[dir];
-          dirY = queuingY + dy[dir];
-          if (dirX >= n || dirY >= m || dirX < 0 || dirY < 0) continue; // out of range
-          if (sketchbook[dirX][dirY] === 0 || sketchbook[dirX][dirY] === 2)
-            continue; // 벽이거나 이미 방문했거나
-          sketchbook[dirX][dirY] = 2;
+          const dirX = queuedX + dx[dir];
+          const dirY = queuedY + dy[dir];
+          if (dirX < 0 || dirX >= n || dirY < 0 || dirY >= m) continue;
+          if (painting[dirX][dirY] !== 1) continue;
+          sizePainting++;
+          painting[dirX][dirY] = 2;
           queue.push([dirX, dirY]);
-          sizeOfPaint++;
         }
-        maxSizePaint = Math.max(maxSizePaint, sizeOfPaint);
+        maxSizePainting = Math.max(maxSizePainting, sizePainting);
       }
     }
   }
 }
-console.log(countOfPaints);
-console.log(maxSizePaint);
+console.log(countPainting, maxSizePainting);
