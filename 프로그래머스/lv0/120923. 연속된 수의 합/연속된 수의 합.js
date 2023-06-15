01fd27f1a43e0fc2b2a0dd25aca63920a1fc1b3d
SangHyeon <38302831+sak5010@users.noreply.github.com>
2023-06-16 00:17:30 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -0,0 +1,15 @@
+// Math.floor(num / 2)만큼의 수 부터 오름차순으로 num의 개수 만큼 더했을 때 total이 되는지 찾으면 된다.
+// total이 되는 녀석들을 배열에 담아 return
+function solution(num, total) {
+    let element = -Math.floor(num / 2);
+    while (true) {
+        let sum = 0;
+        const result = [];
+        for (let i = 0; i < num; i++) {
+            sum += element + i;
+            result.push(element + i);
+        }
+        if (sum === total) return result;
+        element++;
+    }
+}
