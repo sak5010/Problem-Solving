144528d09395972b819ef8e8a590bf8a80ee76c0
SangHyeon <38302831+sak5010@users.noreply.github.com>
2023-07-10 16:58:49 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+    d.sort((a, b) => a - b);
+    let result = 0;
+    for (let i = 0; i < d.length; i++) {
+        if (budget - d[i] >= 0) {
+            budget -= d[i];
+            result++;
+        } else {
+            break;
+        }
+    }
+    return result;
+}
