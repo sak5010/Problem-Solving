718521f6d0b3c6275919727c929997ad745be95c
SangHyeon <38302831+sak5010@users.noreply.github.com>
2023-08-03 16:32:06 +0900

    [Silver II] Title: 종이의 개수, Time: 780 ms, Memory: 103552 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.js
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.js
@@ -7,7 +7,7 @@ const paper = input.map((line) => line.split(" ").map(Number));
 
 const cnt = new Array(3).fill(0); // -1, 0, 1의 개수
 
-const isSame = (x, y, n) => {
+const check = (x, y, n) => {
   for (let i = x; i < x + n; i++) {
     for (let j = y; j < y + n; j++) {
       if (paper[i][j] !== paper[x][y]) return false;
@@ -17,17 +17,17 @@ const isSame = (x, y, n) => {
 };
 
 const solve = (x, y, z) => {
-  if (isSame(x, y, z)) {
+  if (z === 1 || check(x, y, z)) {
     cnt[paper[x][y] + 1] += 1;
     return;
   }
   const n = z / 3;
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      solve(x + i * n, y + j * n, n);
+  for (let i = x; i < x + z; i += n) {
+    for (let j = y; j < y + z; j += n) {
+      solve(i, j, n);
     }
   }
 };
 
 solve(0, 0, n);
-cnt.forEach((c) => console.log(c));
+console.log(cnt.join("\n"));
